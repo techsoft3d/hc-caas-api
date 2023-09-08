d20b97a72456133071dd123d0ea9574e1eb4dd0c
Guido <eric5544@gmail.com>
2023-09-08 06:43:02 -0700

    simplified step export

--- testExportStep.js
+++ testExportStep.js
@@ -2,11 +2,8 @@ const caasClient = require('./api/CaasClient');
 
 (async () => {
     caasClient.init('http://localhost:3001');
-    let info = await caasClient.uploadModelFromFile("./testfiles/axe.CATPART",null,{skipConversion:true});
-    await caasClient.reconvertModel(info.itemid,{conversionCommandLine:["--output_step",""]});
+    let info = await caasClient.uploadModelFromFile("./testfiles/axe.CATPART",null,{conversionCommandLine:["--output_step",""]});
     await caasClient.waitUntilConverted(info.itemid);
-    let modelData = await caasClient.getModelData(info.itemid);
-    console.log(modelData);
     let res = await caasClient.getFileByType(info.itemid, "step", "./output/" + "axe.step");
     console.log(res);
 })();
